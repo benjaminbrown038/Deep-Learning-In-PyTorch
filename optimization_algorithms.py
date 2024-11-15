f0b4520bab27f0b5152c228e1e50dd8181832e19
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-11-15 13:58:46 -0500

    Create optimization_algorithms.py

---
+++ optimization_algorithms.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+
+import os 
+import time
+from typing import Iterable, List, Tuple
+from dataclasses import dataclass
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim 
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+
+class LeNet():
+    def __init__():
+        super().__init__()
+        self._body = nn.Sequential()
+        self._head = nn.Sequential()
+    def forward():
+        return x 
+
+lenet_model = LeNet()
+print(lenet_model)
+
+def get_data():
+    return train_loader, test_loader
+
+@dataclass 
+class SystemConfiguration:
+
+@dataclass 
+class TrainingConfiguration:
+
+def setup_system():
+
+def train():
+    return epoch_loss, epoch_acc
+
+def validate():
+    return test_loss, accuracy/100.0
+
+def main():
+    return model, epoch_train_loss, epoch_train_acc, epoch_test_loss, epoch_test_acc
+
+model = LeNet()
+train_config = TrainingConfiguration()
+optimizer = optim.SGD()
+
+
+
+
+
+        
+        
