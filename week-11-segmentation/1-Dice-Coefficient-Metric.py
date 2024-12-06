19e4d6f645ef6a09a9675408f65958c6c0095a4a
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-12-05 19:21:24 -0500

    Update 1-Dice-Coefficient-Metric.py

--- week-11-segmentation/1-Dice-Coefficient-Metric.py
+++ week-11-segmentation/1-Dice-Coefficient-Metric.py
@@ -1,1 +1,26 @@
+import numpy as np
+import torch
+from dataclasses import dataclass
+import random
 
+@dataclass
+class SystemConfig:
+
+def setup_system():
+
+class ConfusionMatrix:
+    def __init__():
+    def reset():
+    def add():
+    def value():
+
+
+class ConfusionBasedMetric:
+    def __init__():
+    def reset():
+    def add():
+
+
+class DiceCoefficient():
+    def value():
+    
