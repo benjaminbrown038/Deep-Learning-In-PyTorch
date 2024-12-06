39faa1cec91700cc3c12b5d0f55e453c2e240432
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-12-05 19:44:59 -0500

    Update 5-UNet-Using-PyTorch-Hub.py

--- week-11-segmentation/5-UNet-Using-PyTorch-Hub.py
+++ week-11-segmentation/5-UNet-Using-PyTorch-Hub.py
@@ -1,1 +1,19 @@
+from torchvision import models
+import torch
+import torchvision.transforms as T
+from PIL import Image
+import matplotlib.pyplot as plt
+import urllib
+import numpy as np
+import time
+import glob
+import os 
+
+!wget
+!unzip
+!rm
+
+def get_images_and_mask_images():
+
+def brain_mri_abnormally_detection():
 
