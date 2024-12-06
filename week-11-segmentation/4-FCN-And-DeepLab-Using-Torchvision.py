4ededfd4ce40c0cec87470ff215d65f4c0b99283
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-12-05 19:41:47 -0500

    Update 4-FCN-And-DeepLab-Using-Torchvision.py

--- week-11-segmentation/4-FCN-And-DeepLab-Using-Torchvision.py
+++ week-11-segmentation/4-FCN-And-DeepLab-Using-Torchvision.py
@@ -1,1 +1,17 @@
+from torchvision import models
+import torch
+import torchvision.transforms as T
+from PIL import Image 
+import matplotlib.pyplot as plt
+import urllib
+import numpy as np 
+import time
+
+def download_image():
+
+def decode_segmap():
+
+def segment():
+
+def avg_infer_time():
 
