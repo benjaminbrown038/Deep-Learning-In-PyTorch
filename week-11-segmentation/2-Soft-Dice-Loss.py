dd38650ed7b960290861dc75fe6c6e377e072ae6
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-12-05 19:23:06 -0500

    Update 2-Soft-Dice-Loss.py

--- week-11-segmentation/2-Soft-Dice-Loss.py
+++ week-11-segmentation/2-Soft-Dice-Loss.py
@@ -1,1 +1,15 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from dataclasses import dataclass
+import random
 
+@dataclass
+class SystemConfig:
+
+def setup_system():
+
+class SoftDiceLoss():
+    def __init__():
+    def forward():
+    
